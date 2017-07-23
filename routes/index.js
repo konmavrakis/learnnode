5a71ffb8b988ec35e8e5ec7924bd4b7b23e9c95a
Ntinos Mavrakis <konmavrakis@gmail.com>
2017-07-23 16:34:39 +0300

    Routing

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+	const ntinos = {name: ntinos, age: 1234, node: true};
+	//res.json(ntinos);
+	//res.send(req.query.name);
+	res.json(req.query);
+
+});
+
+
+router.get('/name/:name', (req, res) => {
+	const name = [...req.params.name].reverse().join("");
+	res.send(name);
 });
 
 module.exports = router;
