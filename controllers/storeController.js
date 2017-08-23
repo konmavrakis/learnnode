565cf2162b984b47eeb61318c45fbd8013120e11
Ntinos Mavrakis <konmavrakis@gmail.com>
2017-08-23 11:51:59 +0300

    Store Model

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,8 +1,4 @@
-exports.myMiddleware = (req, res, next) =>{
-	req.name = 'Node';
-	next();
-}
-
 exports.homePage = (req, res) => {
-	res.render('index');
+	console.log(req.name);
+    res.render('index');
 }
